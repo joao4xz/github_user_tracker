9acfb4f1fcc200e070124ebd6f2769fb4e90995e
joao4xz <joao.4xz@gmail.com>
2025-01-09 20:54:46 -0300

    feat: add user type

---
+++ src/@types/user.d.ts
@@ -0,0 +1,8 @@
+export interface User {
+  username: string;
+  name: string;
+  image_url: string;
+  repo_num: number;
+  followers_num: number;
+  following_num: number;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import { useState, useEffect } from 'react'
 import { fetchUserData } from './api/github.ts'
+import type { User } from './@types/user.d.ts'
 
 function App() {
-  const [userName, setUsername] = useState('');
-  const [name, setName] = useState('');
-  const [image, setImage] = useState('');
+  const [submitValue, setSubmitValue] = useState('');
   const [inputValue, setInputValue] = useState('');
+  const [user, setUser] = useState<User>({
+    name: '',
+    username: '',
+    image_url: '',
+    repo_num: 0,
+    followers_num: 0,
+    following_num: 0,
+  })
 
   useEffect(() => {
-    if (userName) {
-      fetchUserData(userName).then(userData => {
+    if (submitValue) {
+      fetchUserData(submitValue).then(userData => {
         console.log(userData);
-        userData.name && setName(userData.name);
-        userData.avatar_url && setImage(userData.avatar_url);
+        if (userData) {
+          setUser({
+            name: userData.name,
+            username: userData.login,
+            image_url: userData.avatar_url,
+            repo_num: userData.public_repos,
+            followers_num: userData.followers,
+            following_num: userData.following,
+          })
+        }
       });
     }
-  }, [userName]);
+  }, [submitValue]);
 
   return (
     <div className="h-screen flex flex-col items-center gap-4 p-4">
@@ -27,12 +42,12 @@ function App() {
       <button
         className="bg-sky-600 rounded px-8 py-2 hover:bg-sky-700 text-sky-50"
         type="submit"
-        onClick={() => setUsername(inputValue)}
+        onClick={() => setSubmitValue(inputValue)}
       >
         Add user
       </button>
-      <p>{name}</p>
-      <img src={image} />
+      <p>{user.name}</p>
+      <img src={user.image_url} />
     </div>
   )
 }

c160f793240fa5f60e3dc6586262f4b51ab97e4c
joao4xz <joao.4xz@gmail.com>
2025-01-13 21:59:10 -0300

    feat: add shadcn button, input and toast, add error handling

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import { useState, useEffect } from 'react'
 import { fetchUserData } from './api/github.ts'
 import type { User } from './@types/user.d.ts'
 
+import { Button } from "./components/ui/button"
+import { Input } from "./components/ui/input"
+import { Toaster } from "./components/ui/toaster"
+
+import { useToast } from './hooks/use-toast.ts'
+
 function App() {
   const [submitValue, setSubmitValue] = useState('');
   const [inputValue, setInputValue] = useState('');
   const [users, setUsers] = useState<User[]>([])
+
+  const { toast } = useToast()
 
   useEffect(() => {
     if (submitValue) {
@@ -22,23 +30,45 @@ function App() {
           }]);
         }
       })
+      .catch((err) => {
+        console.error(err.message);
+        let message = ""
+        if (err.response.status === 404) {
+          message = "User doesn't exist"
+        } else if (err.response.status === 403) {
+          message = "API rate limit exceeded"
+        } else {
+          message = err.message
+        }
+        if (err.response) {
+          toast({
+            variant: "destructive",
+            title: "Something went wrong!",
+            description: message,
+          })
+        }
+      });
     }
   }, [submitValue]);
 
   return (
     <div className="h-screen flex flex-col items-center gap-4 p-4">
-      <input
-        className="border-2 border-sky-600"
-        type="text"
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button
-        className="bg-sky-600 rounded px-8 py-2 hover:bg-sky-700 text-sky-50"
-        type="submit"
-        onClick={() => setSubmitValue(inputValue)}
+      <form
+        className="flex flex-col gap-6 max-w-1/4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setSubmitValue(inputValue);
+        }}
       >
-        Add user
-      </button>
+        <Input
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button
+          type="submit"
+        >
+          Add user
+        </Button>
+      </form>
       {
         users.map((user) => {
           return <div key={user.username}>
@@ -47,6 +77,7 @@ function App() {
           </div>
         })
       }
+      <Toaster />
     </div>
   )
 }
